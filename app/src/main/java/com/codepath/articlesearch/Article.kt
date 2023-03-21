1b97a59ee1fad322a28c18d41a77d5a30aa0bd33
Peter Kinyanjui <pkkinyanjui@users.noreply.github.com>
2023-03-20 22:17:54 -0700

    Init

--- app/src/main/java/com/codepath/articlesearch/Article.kt
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -1,1 +1,61 @@
 package com.codepath.articlesearch
+
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+//@Keep
+//@Serializable
+//data class SearchNewsResponse(
+//    @SerialName("response")
+//    val response: BaseResponse?
+//)
+
+@Keep
+@Serializable
+data class SearchNewsResponse(
+    @SerialName("results")
+    val response: List<Article>?
+)
+
+@Keep
+@Serializable
+data class Article(
+    @SerialName("overview")
+    val abstract: String?,
+
+    @SerialName("release_date")
+    val byline: Byline?,
+
+    @SerialName("title")
+    val headline: HeadLine?,
+
+    @SerialName("poster_path")
+    val multimedia: List<MultiMedia>?,
+
+) : java.io.Serializable
+
+@Keep
+@Serializable
+data class HeadLine(
+    @SerialName("title")
+    val main: String
+) : java.io.Serializable
+
+
+@Keep
+@Serializable
+data class Byline(
+    @SerialName("release_date")
+    val original: String? = null
+) : java.io.Serializable
+
+@Keep
+@Serializable
+data class MultiMedia(
+    @SerialName("poster_path")
+    val url: String?
+) : java.io.Serializable
+
+
+
--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+//import com.codepath.articlesearch.BuildConfig.API_KEY
+import com.codepath.articlesearch.BuildConfig.BUILD_TYPE
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.BuildConfig
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 fun createJson() = Json {
     isLenient = true
@@ -20,11 +26,12 @@ fun createJson() = Json {
 }
 
 private const val TAG = "MainActivity/"
-private const val SEARCH_API_KEY = BuildConfig.API_KEY
+private const val SEARCH_API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 private const val ARTICLE_SEARCH_URL =
-    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
+    private val articles = mutableListOf<Article>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
@@ -32,11 +39,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         val view = binding.root
         setContentView(view)
 
+
         articlesRecyclerView = findViewById(R.id.articles)
+
         // TODO: Set up ArticleAdapter with articles
+
+        val articleAdapter = ArticleAdapter(this, articles)
+        articlesRecyclerView.adapter = articleAdapter
 
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
@@ -52,23 +65,38 @@ class MainActivity : AppCompatActivity() {
                 throwable: Throwable?
             ) {
                 Log.e(TAG, "Failed to fetch articles: $statusCode")
+                Log.e("1Connection", response.toString())
+
             }
 
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
-                    // TODO: Create the parsedJSON
 
-                    // TODO: Do something with the returned json (contains article information)
+                        // Do something with the returned json (contains article information)
+                        val parsedJson = createJson().decodeFromString(
+                            SearchNewsResponse.serializer(),
+                            json.jsonObject.toString()
+                        )
 
+                    Log.e("1Connection", parsedJson.toString())
+                    // TODO: Save the articles
+                    parsedJson.response?.let { list ->
+                        articles.addAll(list)
+                    }
                     // TODO: Save the articles and reload the screen
+                    parsedJson.response?.let { list ->
+                        articles.addAll(list)
+
+                        // Reload the screen
+                        articleAdapter.notifyDataSetChanged()
+                        Log.e("1Connection", "successful")
+                    }
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
                 }
             }
-
         })
-
     }
 }
